bc9532cd7aafc94349220e43284399a073e9aa93
SINDHU SEKAR <131760219+sindhu-027@users.noreply.github.com>
2025-10-14 19:33:19 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/clean-street/front-end/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true, // send/receive cookies with each request
-});
-
-export default api;
